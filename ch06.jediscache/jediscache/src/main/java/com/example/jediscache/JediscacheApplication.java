b8317f35ea5a1e9e05ffca16c58151ffe0068d2a
Kim KJ <riqua1@naver.com>
2024-06-05 17:13:58 +0900

    ** Redis Cache 실습 **
    1. Cache 실습

--- ch06.jediscache/jediscache/src/main/java/com/example/jediscache/JediscacheApplication.java
+++ ch06.jediscache/jediscache/src/main/java/com/example/jediscache/JediscacheApplication.java
@@ -1,13 +1,46 @@
 package com.example.jediscache;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JediscacheApplication {
+@RequiredArgsConstructor
+public class JediscacheApplication implements ApplicationRunner {
+
+	private final UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JediscacheApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		userRepository.save(
+				User.builder()
+						.name("kj")
+						.email("kj@naver.com")
+						.build()
+		);
+		userRepository.save(
+				User.builder()
+						.name("kj1")
+						.email("kj1@naver.com")
+						.build()
+		);
+		userRepository.save(
+				User.builder()
+						.name("kj2")
+						.email("kj2@naver.com")
+						.build()
+		);
+		userRepository.save(
+				User.builder()
+						.name("kj3")
+						.email("kj3@naver.com")
+						.build()
+		);
+	}
 }
