82a37d964ce5fa72fa215f22bfb6e4ac3bc7b64c
Kim KJ <riqua1@naver.com>
2024-06-07 19:54:48 +0900

    ** Spring boot Redis cache **
    1. Spring Data Redis, redis template 를 이용한 방식

---
+++ ch07.springcache/cache/src/main/java/com/example/cache/config/RedisConfig.java
@@ -0,0 +1,33 @@
+package com.example.cache.config;
+
+import com.example.cache.domain.entity.User;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+
+        var objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS);
+
+        var template = new RedisTemplate<String, User>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(objectMapper, User.class));
+
+        return template;
+    }
+}
--- ch07.springcache/cache/src/main/java/com/example/cache/domain/service/UserService.java
+++ ch07.springcache/cache/src/main/java/com/example/cache/domain/service/UserService.java
@@ -3,15 +3,34 @@ package com.example.cache.domain.service;
 import com.example.cache.domain.entity.User;
 import com.example.cache.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RedisTemplate<String, User> userRedisTemplate;
 
     public User getUser(final Long id) {
-        return userRepository.findById(id).orElseThrow();
+
+        var key = "users:%d".formatted(id);
+
+        // 1. cache get
+        // opsForValue() , Strings 를 쉽게 Serialize / Deserialize 해주는 Interface
+        var cachedUser = userRedisTemplate.opsForValue().get(key);
+
+        if (cachedUser != null) {
+            return cachedUser;
+        }
+
+        // 2. else db -> cache set
+        User user = userRepository.findById(id).orElseThrow();
+        userRedisTemplate.opsForValue().set(key, user, Duration.ofSeconds(30));
+
+        return user;
     }
 }
